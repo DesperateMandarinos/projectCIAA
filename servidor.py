a1c7f68030c4bbc61b9aa1cb6db5faccd2885a26
MelFadanelli <a01749483@tec.mx>
2023-09-06 19:38:20 -0600

    Co-authored-by: YaritziNicio1 <YaritziNicio1@users.noreply.github.com> Co-authored-by: miklaidm13 <miklaidm13@users.noreply.github.com>

--- servidor.py
+++ servidor.py
@@ -30,6 +30,22 @@ def modeloPrediccion():
 
         return jsonify({'resultado':str(resultado[0])})
 
+@servidorWeb.route('/modeloForm',methods=['POST'])        
+def modeloPrediccion():
+        #procesar los deatos de entrada
+        contenido=request.json
+        print(contenido)
+        datosEntrada = np.array([
+                0.88,	0,	2.6,	0.098,	25,	67,	0.9968, 1, 0.4,
+                contenido['pH'],
+                contenido['sulphates'],
+                contenido['alcohol']
+        ])
+        #utiizar el modelo
+        resultado=dt.predict(datosEntrada.reshape(1,-1))
+
+        return jsonify({'resultado':str(resultado[0])})
+
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
